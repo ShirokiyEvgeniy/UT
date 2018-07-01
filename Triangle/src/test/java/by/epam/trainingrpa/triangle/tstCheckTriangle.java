f8300ebda9e6b54f265f7f9f64d7f7131f2452a2
ShirokiyEvgeniy <fpm.shirokiyES@bsu.by>
2018-07-01 16:36:32 +0300

    Added tstCheckTriangle Updated tstGetSquare

---
+++ Triangle/src/test/java/by/epam/trainingrpa/triangle/tstCheckTriangle.java
@@ -0,0 +1,13 @@
+package by.epam.trainingrpa.triangle;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class tstCheckTriangle {
+
+    @Test
+    public void tstSideCLessThan0() {
+        Triangle tr = new Triangle(4.0, 2.0, -2.0);
+        Assert.assertTrue(tr.checkTriangle());
+    }
+}
--- Triangle/src/test/java/by/epam/trainingrpa/triangle/tstGetSquare.java
+++ Triangle/src/test/java/by/epam/trainingrpa/triangle/tstGetSquare.java
@@ -1,11 +1,12 @@
 package by.epam.trainingrpa.triangle;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class tstGetSquare {
 
     @Test
     public void tstZeroPerimeter() {
-
+        Assert.fail();
     }
 }
