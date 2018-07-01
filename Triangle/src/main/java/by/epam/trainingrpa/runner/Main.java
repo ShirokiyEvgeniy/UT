6be5c4ec217e52f3c537a675e2949221ccf52549
ShirokiyEvgeniy <fpm.shirokiyES@bsu.by>
2018-07-01 15:47:34 +0300

    First try

---
+++ Triangle/src/main/java/by/epam/trainingrpa/runner/Main.java
@@ -0,0 +1,16 @@
+package by.epam.trainingrpa.runner;
+
+import by.epam.trainingrpa.triangle.Triangle;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        Triangle tr = new Triangle(1.0, 2.0, 2.0);
+        System.out.println(tr.checkTriangle());
+        System.out.println(tr.getMessage());
+        System.out.println(tr.detectTriangle());
+    }
+
+}
